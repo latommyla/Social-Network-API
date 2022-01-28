37eb2c57d71a29401cd988bb20e9dcc8a6d1e4eb
Tommy La <tommyl.dmd@gmail.com>
2022-01-27 20:14:55 -0800

    testing connection issues

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mongodb = require('mongodb').MongoClient;
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  useFindAndModify: false,
+mongodb.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-mongoose.set('debug', true);
-
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost: ${PORT}`));
