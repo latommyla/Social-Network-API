ddc7691774c7ff8b4e51e6e1f5cee1ff14d8eeab
Tommy La <tommyl.dmd@gmail.com>
2022-01-27 21:13:50 -0800

    fixing connection issues

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const mongodb = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'));
 
-mongodb.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`Connected on localhost: ${PORT}`));
